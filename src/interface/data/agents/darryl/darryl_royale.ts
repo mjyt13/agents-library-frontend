67b84a8a1151a9654292563b6041fd6e1464cb15
Maxim <maksrabb135@gmail.com>
2026-04-24 01:13:02 +0500

    хз почему-то рояль дэррил не пришился к прошлому коммиту

---
+++ src/interface/data/agents/darryl/darryl_royale.ts
@@ -0,0 +1,11 @@
+import type { Agent } from '@/core/models/agent'
+
+export const darryl_royale: Agent = {
+  id: 'darryl_royale',
+  name: 'Sir Bloody Royale Darryl',
+  description:
+    "The leader of 'The Professionals' can be described as cocky, wisecracking, and scarily indifferent with regards to the suffering of others. He would say he's just a bloke—a bloke that likes to have a bit of fun on the weekend with the local armed tax collectors. He downplays the severity of any heavy situations that come his way, even if he's the last man standing, riddled with bullets, with only seconds left to blow the safe. This gold mask with a crown stencil commemorates The Professionals' first visit to Monaco. Do you know what they call a cheeseburger in Monaco?",
+  comment:
+    "Right now it is the best agent in this game. Price, style, look - in every aspect he's higher than anyone",
+  photos: ['agents/darryl/darryl_royale_sniper.png', 'agents/darryl/darryl_royale_sniper.png'],
+}
